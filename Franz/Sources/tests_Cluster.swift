cb9e76f3780bf0f3469c80d4b1c644497f1311a5
Rui Peres <sauron.is.alive@gmail.com>
2018-12-25 21:11:57 +0000

    Testing collection

--- Franz/Sources/tests_Cluster.swift
+++ Franz/Sources/tests_Cluster.swift
@@ -36,5 +36,24 @@ class ClusterTests: XCTestCase {
         scheduler.advance(by: .seconds(1))
         XCTAssert(counter == 0)
     }
+
+    func test_collection() {
+
+        let cluster = Cluster<String, String>()
+        let scheduler = TestScheduler()
+        let consumer = cluster.makeConsumer(for: "Topic1", on: scheduler)
+        var counter = 0
+
+        consumer.observeValues { value in
+            counter += 1
+            XCTAssert(value == ["Hello1", "Hello2"])
+        }
+
+        cluster.send(message: "Hello1", forTopic: "Topic1")
+        cluster.send(message: "Hello2", forTopic: "Topic1")
+
+        scheduler.advance(by: .seconds(1))
+        XCTAssert(counter == 1)
+    }
 }
 
